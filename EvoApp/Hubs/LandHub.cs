41a7f9bd2ef979869aebc9af9e5f5dced589ba13
Andrei Stepanov <steam.oni.am@gmail.com>
2024-03-26 16:20:19 +0400

    positioning

--- EvoApp/Hubs/LandHub.cs
+++ EvoApp/Hubs/LandHub.cs
@@ -9,7 +9,6 @@ namespace EvoApp.Hubs
 	{
 		public void PlaceItem([FromServices] LandMap land, int itemId, int landX, int landY, int tileX, int tileY)
 		{
-            Console.WriteLine((itemId, landX, landY, tileX, tileY));
 			LandTile landTile = land.GetLandTile(landX, landY);
 			landTile.PlaceItem(new Tree("Oak", tileX, tileY), tileX, tileY);
 			Clients.All.SendAsync("PlacedItem", new { itemId, landX, landY, tileX, tileY });
--- EvoApp/Program.cs
+++ EvoApp/Program.cs
@@ -1,5 +1,4 @@
 using EvoApp.Hubs;
-using EvoApp.Models;
 using EvoApp.Services;
 
 namespace EvoApp
@@ -22,23 +21,12 @@ namespace EvoApp
 				pattern: "{controller=Home}/{action=Index}"
 				);
 
-			app.MapGet("api/land", (LandMap landMap) =>
+#if DEBUG
+			app.MapGet("api/test", (LandMap landMap) =>
 			{
-                foreach (var tile in landMap.LandTiles)
-                {
-					foreach (WorldObject? grid in tile.TileGrid)
-                    {
-						Console.ForegroundColor = ConsoleColor.Green;
-                        Console.WriteLine($"Colored name: {grid?.Name}");
-					}
-					Console.ResetColor();
-					Console.WriteLine(tile.TileGrid[0,0]?.Name ?? "None");
-					Console.WriteLine(tile.TileGrid[0,1]?.Name ?? "None");
-					Console.WriteLine(tile.TileGrid[0,2]?.Name ?? "None");
-					
-                }
+                
 			});
-
+#endif
 			app.MapHub<LandHub>("/land");
 
 			app.Run();
